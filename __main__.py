925073b90ef9f15b57d09e30e66a32c88b2742e4
Thomasisamzing <82945745+Thomas-is-amazing@users.noreply.github.com>
2024-06-22 16:30:50 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from sense_hat import SenseHat  # To access Sense HAT sensor data
+`from sense_hat import SenseHat  # To access Sense HAT sensor data
 import time  # To use time functions for delays
 import math  # To perform mathematical operations
 import cmath  # For handling complex numbers
@@ -50,42 +50,3 @@ print("RMS Acceleration Z: {:.2f} g".format(rms_accel_z))
 print("RMS Gyroscope X: {:.2f} °/s".format(rms_gyro_x))
 print("RMS Gyroscope Y: {:.2f} °/s".format(rms_gyro_y))
 print("RMS Gyroscope Z: {:.2f} °/s".format(rms_gyro_z))
-
-# Helper function for FFT
-def fft(x):
-    N = len(x)
-    if N <= 1:
-        return x
-    even = fft(x[0::2])
-    odd = fft(x[1::2])
-    T = [cmath.exp(-2j * cmath.pi * k / N) * odd[k] for k in range(N // 2)]
-    return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
-
-# Perform FFT analysis on accelerometer data
-def fft_magnitudes(values, sampling_rate):
-    N = len(values)
-    if N == 0:
-        return [], []
-    
-    f_values = fft(values)
-    magnitudes = [abs(f) for f in f_values[:N // 2]]
-    frequencies = [i * sampling_rate / N for i in range(N // 2)]
-    return frequencies, magnitudes
-
-# Convert data to complex format for FFT
-def convert_to_complex(data_list):
-    return [complex(v, 0) for v in data_list]
-
-accel_x_complex = convert_to_complex(accel_data['x'])
-accel_y_complex = convert_to_complex(accel_data['y'])
-accel_z_complex = convert_to_complex(accel_data['z'])
-
-# Get frequency and magnitude for X axis
-frequencies_x, magnitudes_x = fft_magnitudes(accel_x_complex, sampling_rate)
-
-print("Frequency domain analysis (Acceleration X):")
-for freq, magnitude in zip(frequencies_x, magnitudes_x):
-    if freq > 0:
-        print("Frequency: {:.2f} Hz, Magnitude: {:.2f}".format(freq, magnitude))
-
-# Similarly, you can analyze frequencies_y and frequencies_z
